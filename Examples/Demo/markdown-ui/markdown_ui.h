18cba1a59a036c665c036073a3a415a7070584bc
Eugene Martinson <emartinson@barevtech.com>
2023-08-13 05:35:51 +0300

    add framework

---
+++ Examples/Demo/markdown-ui/markdown_ui.h
@@ -0,0 +1,13 @@
+//FILENAME
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for markdown_ui.
+FOUNDATION_EXPORT double markdown_uiVersionNumber;
+
+//! Project version string for markdown_ui.
+FOUNDATION_EXPORT const unsigned char markdown_uiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <markdown_ui/PublicHeader.h>
+
+
